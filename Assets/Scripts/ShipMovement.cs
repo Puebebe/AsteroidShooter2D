b92fc4ffccc1cfd5b0bc04e64a99584001c5c3b3
Unknown <kawaxbart@gmail.com>
2019-05-31 21:58:31 +0200

    Add ship steering

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class ShipMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float forwardSpeed;
+    [SerializeField] float rotationSpeed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        float forward = Mathf.Clamp01(Input.GetAxis("Vertical")) * Time.deltaTime * forwardSpeed;
+        float rotation = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+        
+        rb.velocity += (Vector2)transform.up * forward;
+        transform.Rotate(new Vector3(0, 0, rotation));
+    }
+}
