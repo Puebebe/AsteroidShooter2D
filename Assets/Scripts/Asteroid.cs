8691b2eb679725df362e7b93944d3212fb265400
Unknown <kawaxbart@gmail.com>
2019-06-02 21:28:46 +0200

    Add destroying asteroid after collision with other asteroid

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -2,9 +2,23 @@
 
 public class Asteroid : MonoBehaviour
 {
+    [HideInInspector] public bool doNothing = false;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (doNothing)
+            return;
+
         Debug.Log("trigger (asteroid)");
+
+        Asteroid other = collision.GetComponent<Asteroid>();
+
+        if (other != null)
+        {
+            other.doNothing = true;
+            Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
