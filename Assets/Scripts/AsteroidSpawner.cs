1a91f97bec90c8775f00f80ad372bab54a6ea1a2
Unknown <kawaxbart@gmail.com>
2019-06-03 05:45:55 +0200

    Add spawning asteroids on grid

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject asteroidPrefab;
+    [SerializeField] int gridSize;
+
+    void Start()
+    {
+        for (int x = -gridSize / 2 ; x < gridSize / 2; x++)
+        {
+            for (int y = -gridSize / 2; y < gridSize / 2; y++)
+            {
+                Instantiate(asteroidPrefab, new Vector2(x + 0.5f, y + 0.5f), Quaternion.identity);
+            }
+        }
+    }
+}
