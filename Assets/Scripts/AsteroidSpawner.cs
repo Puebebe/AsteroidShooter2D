6dd65a5a24bb93e41b72b07bf017066189aec5a1
Unknown <kawaxbart@gmail.com>
2019-06-04 13:47:34 +0200

    Restrict asteroid spawn area to be outside of view

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -6,15 +6,23 @@ public class AsteroidSpawner : MonoBehaviour
 {
     [SerializeField] GameObject asteroidPrefab;
     [SerializeField] int gridSize;
+    [SerializeField] Camera cam;
 
     void Start()
     {
         StartCoroutine(StartSmoothly());
 
+        Vector2 camPos = cam.transform.position;
+        float xView = cam.orthographicSize * cam.aspect;
+        float yView = cam.orthographicSize;
+
         for (int x = -gridSize / 2 ; x < gridSize / 2; x++)
         {
             for (int y = -gridSize / 2; y < gridSize / 2; y++)
             {
+                if (x + 0.5f < camPos.x + xView && x + 0.5f > camPos.x - xView && y + 0.5f < camPos.y + yView && y + 0.5f > camPos.y - yView)
+                    continue;
+                
                 GameObject asteroid = Instantiate(asteroidPrefab, new Vector2(x + 0.5f, y + 0.5f), Quaternion.identity);
                 asteroid.GetComponent<Rigidbody2D>().velocity = Random.insideUnitCircle;
             }
